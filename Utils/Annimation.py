89e1cd1d30a370a3e1c6d67499c897774d0e073e
fagundes <77056885+LeonardoFagundesJr@users.noreply.github.com>
2022-01-18 10:37:47 -0300

    Update Annimation.py

--- Utils/Annimation.py
+++ Utils/Annimation.py
@@ -1,10 +1,12 @@
 
 #---
 def updateCurve(w):
-    ax.clear()
-    global i,df
+    
+    global i,df,ax
+    
+#     ax.clear()
 
-    i += 8
+#     i += 8
 
     ax.set_xlim3d([-2.5, 2.5])
     ax.set_xlabel('$x~$[m]',labelpad=10)
@@ -17,21 +19,23 @@ def updateCurve(w):
 
     ax.set_title('Route Performed by the ArDrone')
     
-    if i > 0 and i < 450:
-        ax.scatter3D(df['A.pPosXd[0]'][[0]], df['Xd[1]'][[0]],
-                  df['Xd[2]'][[0]], marker='o', alpha = 0.5, color='red')        
-    elif i > 450 and i < 900:
-        ax.scatter3D(df['A.pPosXd[0]'][[0,500]], df['Xd[1]'][[0,500]],
-                  df['Xd[2]'][[0,500]], marker='o', alpha = 0.5, color='red')
-    elif i > 900 and i < 1350:
-        ax.scatter3D(df['A.pPosXd[0]'][[0,500,1000]], df['Xd[1]'][[0,500,1000]],
-                  df['Xd[2]'][[0,500,1000]], marker='o', alpha = 0.5, color='red')
-    else:
-        ax.scatter3D(df['A.pPosXd[0]'][[0,500,1000,1500]], df['Xd[1]'][[0,500,1000,1500]],
-                  df['Xd[2]'][[0,500,1000,1500]], marker='o', alpha = 0.5, color='red')        
+#     if i > 0 and i < 450:
+#         ax.scatter3D(df['A.pPosXd[0]'][[0]], df['Xd[1]'][[0]],
+#                   df['Xd[2]'][[0]], marker='o', alpha = 0.5, color='red')        
+#     elif i > 450 and i < 900:
+#         ax.scatter3D(df['A.pPosXd[0]'][[0,500]], df['Xd[1]'][[0,500]],
+#                   df['Xd[2]'][[0,500]], marker='o', alpha = 0.5, color='red')
+#     elif i > 900 and i < 1350:
+#         ax.scatter3D(df['A.pPosXd[0]'][[0,500,1000]], df['Xd[1]'][[0,500,1000]],
+#                   df['Xd[2]'][[0,500,1000]], marker='o', alpha = 0.5, color='red')
+#     else:
+#         ax.scatter3D(df['A.pPosXd[0]'][[0,500,1000,1500]], df['Xd[1]'][[0,500,1000,1500]],
+#                   df['Xd[2]'][[0,500,1000,1500]], marker='o', alpha = 0.5, color='red')        
     
 
-    ax.plot3D(df['A.pPosX[0]'][0:i], df['X[1]'][0:i], df['X[2]'][0:i], label='Position')
+#     ax.plot3D(df['A.pPosX[0]'][0:i], df['X[1]'][0:i], df['X[2]'][0:i], label='Position')
+
+    ax.plot3D(df['A.pPosX[0]'][w], df['X[1]'][w], df['X[2]'][w], label='Position')
 
     ax.legend(['$\mathbf{x}$','$\mathbf{x_d}$'],loc='center right', bbox_to_anchor=(1.05, 0.5))
 
