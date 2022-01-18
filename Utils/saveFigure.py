1f8808acc9dd1938b4aedfe05647b39caccafef7
fagundes <77056885+LeonardoFagundesJr@users.noreply.github.com>
2022-01-18 10:02:31 -0300

    Update saveFigure.py

--- Utils/saveFigure.py
+++ Utils/saveFigure.py
@@ -4,5 +4,7 @@
 def saveFig(fig,nameFig): 
     # ------- the name must contain the figure format:
     # e.g., nameFig = 'Position.png' or nameFig = 'linearVelocity.eps'
-
+    import os
+    
+    nameFig = os.path.join("/content/Figures/",nameFig)
     fig.savefig(nameFig, dpi=300, bbox_inches='tight')
