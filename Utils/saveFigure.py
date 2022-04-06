4fac8e60e64c3643532263d17f5180b607778238
fagundes <77056885+LeonardoFagundesJr@users.noreply.github.com>
2022-04-05 22:37:35 -0300

    Update saveFigure.py
    Melhoria da função saveFig

--- Utils/saveFigure.py
+++ Utils/saveFigure.py
@@ -1,10 +1,25 @@
 
 #---
 # Save Figure:
-def saveFig(fig,nameFig): 
+def saveFig_old(fig,nameFig): 
     # ------- the name must contain the figure format:
     # e.g., nameFig = 'Position.png' or nameFig = 'linearVelocity.eps'
     import os
     
     nameFig = os.path.join("/content/Figures/",nameFig)
     fig.savefig(nameFig, dpi=300, bbox_inches='tight')
+
+    
+#---
+# Save Figure:
+def saveFig(fig,nameFig): 
+    # ------- the name must contain the figure format:
+    # e.g., nameFig = 'Position.png' or nameFig = 'linearVelocity.eps'
+    import os
+
+    if not(os.path.isdir('../content/Figures/')):
+        os.mkdir('../content/FiguresA/')
+
+    nameFig = os.path.join("../content/Figures/",nameFig)        
+
+    fig.savefig(nameFig, dpi=600, bbox_inches='tight')
